a1432a1b3479e585c067df3b1f65a49850a7d4b3
bluesatch <satchwerk@gmail.com>
2025-02-14 14:08:31 -0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,131 @@
+/**
+ * Array of albums
+ *  id, artist, title, label, stars, genre, group "new or fave"
+ * 
+ */
+
+const albums = [
+    {
+        id: "01241-41418-1",
+        artist: 'a tribe called quest',
+        title: 'the low end theory',
+        label: 'jive',
+        stars: 5,
+        genre: 'hip hop',
+        yrReleased: 1991,
+        group: 'fave'
+    },
+    {
+        id: '602475686750',
+        artist: 'kendrick lamar',
+        title: 'gnx',
+        label: 'interscope',
+        stars: 4,
+        genre: 'hip hop',
+        yrReleased: 2025,
+        group: 'new'
+    },
+    {
+        id: 'ART-014',
+        artist: 'snoh aalegra',
+        title: 'temporary highs in the violet skies',
+        label: 'ARTIum',
+        stars: 3,
+        genre: 'r&b/soul',
+        yrReleased: 2022,
+        group: 'new'
+    },
+    {
+        id: 'PD-2-3007',
+        artist: 'james brown',
+        title: 'the payback',
+        label: 'polydor',
+        stars: 4,
+        genre: 'funk',
+        yrReleased: 1973,
+        group: 'fave'
+    },
+    {
+        id: 'CR00518',
+        artist: 'johnny hammond',
+        title: 'gears',
+        label: 'milestone',
+        stars: 5,
+        genre: '#BAM',
+        yrReleased: 1975,
+        group: 'new'
+    },
+    {
+        id: 'AKR-015',
+        artist: 'lynda dawn',
+        title: 'at first light',
+        label: 'akashik',
+        stars: 4,
+        genre: 'soul',
+        yrReleased: 2019,
+        group: 'new'
+    },
+    {
+        id: 'MOVLP1124',
+        artist: 'erykah badu',
+        title: "Mama's Gun",
+        label: 'motown',
+        stars: 5,
+        genre: 'soul',
+        yrReleased: 2000,
+        group: 'fave'
+    },
+    {
+        id: 'BST-31875',
+        artist: 'donald byrd',
+        title: 'kofi',
+        label: 'blue note',
+        stars: 5,
+        genre: '#BAM',
+        yrReleased: 1995,
+        group: 'new'
+    },
+    {
+        id: 'BIN-LA368-G',
+        artist: 'donald byrd',
+        title: 'stepping into tomorrow',
+        label: 'blue note',
+        stars: 5,
+        genre: '#BAM',
+        yrReleased: 1975,
+        group: 'fave'
+    },
+    {
+        id: 'RM039',
+        artist: 'french kiwi juice',
+        title: 'fkj',
+        label: 'roche musique',
+        stars: 3,
+        genre: 'synth-pop',
+        yrReleased: 2017,
+        group: 'new'
+    },
+    {
+        id: 'LVR04335',
+        artist: 'common & pete rock',
+        title: 'the auditorium vol. 1',
+        label: 'loma vista',
+        stars: 4,
+        genre: 'hip hop',
+        yrReleased: 2024,
+        group: 'fave'
+    },
+    {
+        id: 'VBR38116',
+        artist: 'roy hargrove',
+        title: 'hard groove',
+        label: 'verve',
+        stars: 5,
+        genre: '#BAM',
+        yrReleased: 2023,
+        group: 'fave'
+    }
+]
+
+
+
